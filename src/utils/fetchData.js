40582463a83b6fda7af077f247c23f1a283ec840
shivaprasad369 <122955366+shivaprasad369@users.noreply.github.com>
2024-04-26 12:05:43 +0530

    add

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': "4daa9bea11msh78f57d6671a1229p102c7ajsne64b93e90ce4",
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': '4daa9bea11msh78f57d6671a1229p102c7ajsne64b93e90ce4',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
